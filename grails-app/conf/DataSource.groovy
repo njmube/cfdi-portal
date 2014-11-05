9745ed2e395d2794bf917d95e5708f5923a37483
Ruben Cancino <rubencancino6@gmail.com>
2014-11-05 09:33:29 -0600

    Database update

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -26,7 +26,7 @@ environments {
             //url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
             
             pooled = true
-            dbCreate="create-drop"
+            dbCreate="update"
             url = "jdbc:mysql://localhost/cfdi?autoReconnect=true"
             driverClassName = "com.mysql.jdbc.Driver"
             dialect = org.hibernate.dialect.MySQL5InnoDBDialect
